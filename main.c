77225b87d5135f38b5f5734ee30afc4ba22ffc98
Whitepad <padmareddy15689@gmail.com>
2020-09-08 15:28:29 +0530

    code documentation

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ int analogValue;
 
 int main(void)
 {
-	RCC->AHB1ENR |= (1<<0);		//ENABLE CLOCK FOR PORT A
-	GPIOA->MODER |= (3<<0);  //SETTING PA0 TO ANALOG
+	RCC->AHB1ENR |= (1<<0);				 //ENABLE CLOCK FOR PORT A
+	GPIOA->MODER |= (3<<0); 	  	 //SETTING PA0 TO ANALOG
 	
 	//CONFIGURE ADC1
-	RCC->APB2ENR |= (1<<8);  //ADC1 CLOCK ENABLED
-	ADC1->CR2  = 0	;    		 //DISABLE ADC1
+	RCC->APB2ENR |= (1<<8); 		   //ADC1 CLOCK ENABLED
+	ADC1->CR2  = 0	;    		 			 //DISABLE ADC1
 	ADC1->SQR1 = 1;
-	ADC1->CR2  |= (1<<0);			//ENABLE ADC1
+	ADC1->CR2  |= (1<<0);			     //ENABLE ADC1
 
 		while(1)
 		{
